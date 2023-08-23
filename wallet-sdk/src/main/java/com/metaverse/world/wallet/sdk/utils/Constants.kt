7376434e8a22e84b3d1943109f2cbbd3350a3e5e
sg.park <psgxxx@nm-metaworld.com>
2023-08-23 14:21:28 +0900

    remove - Constant

--- wallet-sdk/src/main/java/com/metaverse/world/wallet/sdk/utils/Constants.kt
+++
@@ -1,4 +0,0 @@
-package com.metaverse.world.wallet.sdk.utils
-
-internal const val STRING_KEY_API = "com.metaverse.world.fncy.ApiKey"
-internal const val STRING_KEY_BASE_URL = "com.metaverse.world.fncy.BaseUrl"
